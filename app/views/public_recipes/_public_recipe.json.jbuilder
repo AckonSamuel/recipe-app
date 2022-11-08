ef8b1c91bf53e204e4437748a64fa4ac23a97175
Samuel Ackon <ackons433@gmail.com>
2022-11-08 09:48:01 +0000

    Add public_recipe scaffold generated files

---
+++ app/views/public_recipes/_public_recipe.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! public_recipe, :id, :created_at, :updated_at
+json.url public_recipe_url(public_recipe, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :public_recipes
   resources :foods
   resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

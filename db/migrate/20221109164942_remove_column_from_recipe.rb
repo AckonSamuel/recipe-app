d7bc453a32de6175b5bd14dfc603fcb46d2c1771
Samuel Ackon <ackons433@gmail.com>
2022-11-09 18:56:47 +0000

    Add form for new recipe input

--- db/migrate/20221109164942_remove_column_from_recipe.rb
+++ db/migrate/20221109164942_remove_column_from_recipe.rb
@@ -1,8 +1,8 @@
 class RemoveColumnFromRecipe < ActiveRecord::Migration[7.0]
   def change
     remove_column :recipes, :preparation_time, :datetime
-    remove_column :recipes, :preparation_time, :datetime
-    add_column :recipes, :cooking_time, :string
+    remove_column :recipes, :cooking_time, :datetime
+    add_column :recipes, :preparation_time, :string
     add_column :recipes, :cooking_time, :string
   end
 end

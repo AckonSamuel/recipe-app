d4441597fd99189a8f2f165c4be4bd224795e4e5
Samuel Ackon <ackons433@gmail.com>
2022-11-10 16:21:42 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # devise_for :users
-  # resources :users
+  resources :users
   # resources :general_shopping_lists
   # resources :public_recipes
   # resources :foods
-  # resources :recipes
+  resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
